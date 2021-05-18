7d267ac072e89e5b952c41c77f261218be665d88
ojanu <ond.janu@gmail.com>
2021-05-18 19:55:10 +0200

    Novy soubor Test.py

---
+++ Test.py
@@ -0,0 +1,7 @@
+
+list_jmen = ["Ondra", "Petr", "Lucie", "Karel", "Franta"]
+
+for jmeno in list_jmen:
+    print("Toto vypisu pro kazde jmeno: ")
+    if jmeno.startswith("K"):
+        print(jmeno)
