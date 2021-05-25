6ff2ba943c3c226ea25b8c4845483908a36cf723
ojanu <ond.janu@gmail.com>
2021-05-25 17:49:50 +0200

    Úprava souboru Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,13 @@
-
-list_jmen = ["Ondra", "Petr", "Lucie", "Karel", "Franta"]
-
-for jmeno in list_jmen:
-    print("Toto vypisu pro kazde jmeno: ")
-    if jmeno.startswith("K"):
-        print(jmeno)
+start = int(input("START: "))
+stop = int(input("STOP: "))
+divisor = int(input("DIVISOR: "))
+pr_list = []
+if divisor == 0:
+  print("Cannot divide by zero")
+  exit()
+# generování sbírky
+print("Numbers in range", (start, (stop + 1)), "divisible by", divisor, ":")
+for i in range(start, (stop + 1)):
+  if i % divisor == 0:
+    pr_list.append(i)
+    print(pr_list)
