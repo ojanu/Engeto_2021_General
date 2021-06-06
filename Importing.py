dbf40d2eceda50c6c2ae959ee9441acdb965103a
ojanu <ond.janu@gmail.com>
2021-06-06 09:58:54 +0200

    reformat file with importing testing

--- Importing.py
+++ Importing.py
@@ -1,4 +1,5 @@
 import os
+
 start_dir = input('Starting dir:')
 target = input('Target:')
 paths = []
@@ -6,7 +7,7 @@ dirs_to_search = [start_dir]
 while dirs_to_search:
     current_dir = dirs_to_search.pop(0)
     for item in os.listdir(current_dir):
-        item_path = os.path.join(current_dir,item)
+        item_path = os.path.join(current_dir, item)
         if item.split('.')[0] == target:
             paths.append(item_path)
         if os.path.isdir(item_path):
